a811ba8899283057f293efd5c2d0cebbe32a88b4
Jacobb002 <poltorakjakub02@gmail.com>
2022-02-04 11:49:12 +0100

    Add date

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,5 +1,6 @@
 function startTime() {
     const today = new Date();
+    
     const clock = {
         hour: today.getHours(),
         minute: today.getMinutes(),
@@ -8,6 +9,16 @@ function startTime() {
     clock.hour = check(clock.hour);
     clock.minute = check(clock.minute);
     clock.second = check(clock.second);
-    document.getElementById('clock').innerHTML = clock.hour + ':' + clock.minute + ':' + clock.second;
+
+    const date = {
+        day: today.getDay(),
+        month: today.getMonth(),
+        year: today.getFullYear(),
+    }
+    date.day = check(date.day);
+    date.month = check(date.month);
+
+    document.getElementById('clock_time').innerHTML = clock.hour + ':' + clock.minute + ':' + clock.second;
+    document.getElementById('clock_date').innerHTML = date.day + '.' + date.month + '.' + date.year;
     setTimeout(startTime, 1000);
 }
